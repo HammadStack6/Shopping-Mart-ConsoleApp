cb6d525c7a0de99d3031d3106c9824bdee4ea062
hammad <77hammdkhan@gmail.com>
2025-04-23 14:00:12 +0500

    Add project files.

---
+++ Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopping_Mart_ConsoleApp
+{
+    class Product
+    {
+        string[] productNames = { "Milk", "Bread", "Eggs", "Juice" };
+        int[] productPrices =   { 20, 30, 10, 50 };
+    }
+    }
---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Shopping_Mart_ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+    
+
+            string[] Products = { "Banana", "Milk", "Bread", "Eggs", "Juice" }; // 5 products
+            int[] Prices = { 20, 30, 40, 10, 50 }; // Prices for each product
+            List<(string name, int price)> cart = new List<(string name, int price)>(); //this
+
+            Console.WriteLine("----------WELCOME TO THE MART----------");
+
+            bool condition = true;
+            while (condition) { 
+            
+            for (int i = 0; i < Products.Length; i++)
+                {
+                    Console.WriteLine($"{i+1}. {Products[i]} - {Prices[i]}");
+
+                }
+
+                Console.WriteLine("enter the product number you want ");
+                
+                int input = Convert.ToInt32(Console.ReadLine());
+                if (input <1 || input > 5)
+                {
+                    Console.WriteLine("in valid product--- SELECT THE PRODUCT PROPERLY");
+                 
+                }
+                Console.WriteLine($"THE PRODUCT YOU SELECTED IS {Products[input-1]}  for {Prices[input-1]} Rs");
+                Console.WriteLine("do you want to see your cart items  yes/no ");
+                String agree1 = Console.ReadLine().ToLower();
+                if (agree1 == "no")
+                {
+                    condition = true;
+                }
+                cart.Add((Products[input - 1], Prices[input - 1])); //this
+
+                if (agree1 == "yes")
+                {
+                    Console.WriteLine("\n--- Your Cart Items ---"); //this
+                    foreach (var c in cart)
+                    {
+                        Console.WriteLine($"{c.name} , {c.price} Rs");
+                    }
+                }//this
+
+
+
+
+
+                Console.WriteLine("Do you want to continue SHOPPING type yes/no");
+                String agree = Console.ReadLine().ToLower();
+                if (agree =="no")
+                {
+                    Console.WriteLine("Thank you for visiting");
+                    condition = false;
+                }
+            
+            
+            
+            
+            
+            }
+
+
+
+        }
+    }
+}
